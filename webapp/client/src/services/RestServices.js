9421be1896f9b15311a0f00b66facc78008547d7
eniolastyle <amiolastyle@gmail.com>
2023-06-08 07:27:16 +0100

    microservice with vue and node

--- webapp/client/src/services/RestServices.js
+++ webapp/client/src/services/RestServices.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 let dev = true;
 
-let serverUrl = dev ? 'http://localhost:3001' : 'http://<SERVER_ALB_URL>'; //this value is replaced by AWS CodeBuild
+let serverUrl = 'http://<SERVER_ALB_URL>'; //this value is replaced by AWS CodeBuild
 
 export default {
   async getAllProducts() {
