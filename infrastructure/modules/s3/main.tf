89b941a612aa3e4f1ba8da541b51ce3994ff8bcd
eniolastyle <amiolastyle@gmail.com>
2023-06-09 16:06:42 +0100

    microservice with vue and node

--- infrastructure/modules/s3/main.tf
+++ infrastructure/modules/s3/main.tf
@@ -13,7 +13,7 @@ resource "aws_s3_bucket" "s3_bucket" {
 
 resource "aws_s3_bucket_ownership_controls" "s3_bucket_ownership" {
   count = var.public_s3 == true ? 1 : 0
-#  force_destroy = true
+  #  force_destroy = true
   bucket = aws_s3_bucket.s3_bucket.id
   rule {
     object_ownership = "BucketOwnerPreferred"
@@ -22,7 +22,7 @@ resource "aws_s3_bucket_ownership_controls" "s3_bucket_ownership" {
 
 resource "aws_s3_bucket_public_access_block" "s3_bucket_access" {
   count = var.public_s3 == true ? 1 : 0
-#  force_destroy = true
+  #  force_destroy = true
   bucket = aws_s3_bucket.s3_bucket.id
 
   block_public_acls       = false
@@ -33,7 +33,7 @@ resource "aws_s3_bucket_public_access_block" "s3_bucket_access" {
 
 resource "aws_s3_bucket_acl" "s3_bucket_acl" {
   count = var.public_s3 == true ? 1 : 0
-#  force_destroy = true
+  #  force_destroy = true
   depends_on = [
     aws_s3_bucket_ownership_controls.s3_bucket_ownership,
     aws_s3_bucket_public_access_block.s3_bucket_access,
@@ -41,4 +41,32 @@ resource "aws_s3_bucket_acl" "s3_bucket_acl" {
 
   bucket = aws_s3_bucket.s3_bucket.id
   acl    = "public-read"
+}
+
+resource "aws_s3_bucket_policy" "asset_bucket_policy" {
+  count = var.public_s3 == true ? 1 : 0
+  depends_on = [
+    aws_s3_bucket_ownership_controls.s3_bucket_ownership,
+    aws_s3_bucket_public_access_block.s3_bucket_access,
+  ]
+
+  bucket = aws_s3_bucket.s3_bucket.id
+  policy = jsonencode({
+    Id      = "BucketPolicy"
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid       = "AllAccess"
+        Effect    = "Allow"
+        Principal = "*"
+        Action = [
+          "s3:*"
+        ]
+        Resource = [
+          "${aws_s3_bucket.s3_bucket.arn}",
+          "${aws_s3_bucket.s3_bucket.arn}/*"
+        ]
+      }
+    ]
+  })
 }
